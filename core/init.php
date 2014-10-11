3268d0f0263528ca7778ffb7e2928c1bf3d0cce1
Chris Tran <teboedits@gmail.com>
2014-10-10 23:02:39 -0700

    Autoload Fix

--- core/init.php
+++ core/init.php
@@ -23,7 +23,7 @@ $GLOBALS['config'] = array(
 );
 
 spl_autoload_register(function($class) {
-    require_once 'classes/' . $class . '.php';
+    require_once 'classes/' . strtolower($class) . '.php';
 });
 
 require_once 'functions/sanitize.php';
